5973922d2813eec9c972986f53861a79b3ad6a36
kimnoeun <1203shdms@naver.com>
2019-05-24 16:12:57 +0900

    둘이꺼 합쳐놓은거 올리니까 정민씨....박스 수정 좀 해주십시오...박스를 지나갈때 습득하는거 말고 자리 확정했을때만 습득하는거랑 한번 습득했으면 다시 안생기는거요

---
+++ chessTest 김노은(말관련)+ 최정민(상자관련)/chesstest/Assets/ChessTest/scripts/ChessPiece.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace ChessTest {
+    public class ChessPiece : MonoBehaviour {
+        public bool isWhite;
+        public int X, Z;
+    }
+}
