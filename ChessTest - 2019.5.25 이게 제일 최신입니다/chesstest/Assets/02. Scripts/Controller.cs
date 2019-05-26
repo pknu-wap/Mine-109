64605dfdd345dc6ea2421a9e0a4a32b40ce5240c
cnwcjm9918 <cnwcjm9918@gmail.com>
2019-05-27 00:21:12 +0900

    복원 전 커밋 4

--- ChessTest - 2019.5.25 이게 제일 최신입니다/chesstest/Assets/02. Scripts/Controller.cs
+++ ChessTest - 2019.5.25 이게 제일 최신입니다/chesstest/Assets/02. Scripts/Controller.cs
@@ -84,8 +84,9 @@ namespace ChessTest
             else if(other.gameObject.CompareTag("Bomb"))
             {
                 SpawnMgrbomb manager2 = SpawnMgrbomb.Instancebomb;
-                manager2.StartCoroutine(manager2.ReviveBomb(other.gameObject, BombSpawnTime));
-            }
+                manager2.StartCoroutine(manager2.ReviveBomb(other.gameObject, BombSpawnTime));            }
+        }
+
+      
         }
     }
-}
--- ChessTest - 2019.5.25 이게 제일 최신입니다/chesstest/Assets/02. Scripts/SpawnMgr.cs
+++ ChessTest - 2019.5.25 이게 제일 최신입니다/chesstest/Assets/02. Scripts/SpawnMgr.cs
@@ -51,7 +51,9 @@ public class SpawnMgr : MonoBehaviour
             //yield return new WaitForSeconds(createTime);
             int idx = (int)(Random.value * (transforms.Count - 1));
             Instantiate(boxPrefab, transforms[idx].position, transforms[idx].rotation);
-            transforms.RemoveAt(idx);           
+            transforms.RemoveAt(idx);
+            Debug.Log("idx = " + idx);
+            Debug.Log("transform.Count =" + transforms.Count);
             
             //4개 생성 완료+리스트 개수 0
         }
